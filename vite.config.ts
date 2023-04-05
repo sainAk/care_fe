b370941399af3612d71c3db79079060493e4b14d
Aakash Singh <mail@singhaakash.dev>
2023-04-06 00:37:08 +0530

    use proper workarounds in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
-import { resolve } from "path";
-import { promises as fs } from "fs";
-
+import fs from "fs";
+import * as esbuild from "esbuild";
 
 export default defineConfig({
   envPrefix: "REACT_",
   plugins: [
     react(),
     VitePWA({
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'service-worker.ts',
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "service-worker.ts",
       injectRegister: null,
       injectManifest: {
-        maximumFileSizeToCacheInBytes: 7000000
+        maximumFileSizeToCacheInBytes: 7000000,
       },
       manifest: {
         name: "Care",
@@ -38,11 +37,31 @@ export default defineConfig({
             src: "https://cdn.coronasafe.network/care-manifest/images/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          }
+          },
         ],
       },
     }),
   ],
+  server: {
+    port: 4000,
+    proxy: {
+      "/api": {
+        target: "https://careapi.coronasafe.in",
+        changeOrigin: true,
+      },
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        // replace global with the standard way in dependencies
+        global: "globalThis",
+      },
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
   build: {
     outDir: "build",
     rollupOptions: {
@@ -53,11 +72,26 @@ export default defineConfig({
           }
         },
       },
+      plugins: [
+        {
+          // workaround to accept jsx syntax in js files
+          name: "load-js-files-as-jsx",
+          async load(id) {
+            if (id.match(/node_modules.*src.*\.js$/)) {
+              console.warn("Loading", id);
+              return esbuild.transformSync(fs.readFileSync(id, "utf8"), {
+                loader: "jsx",
+              });
+            }
+          },
+        },
+      ],
     },
     commonjsOptions: {
-      // workaround for react-phone-input-2 https://github.com/vitejs/vite/issues/2139#issuecomment-1405624744
+      // workaround for packages that have mixed ES modules and CommonJS
       defaultIsModuleExports(id) {
         try {
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
           const module = require(id);
           if (module?.default) {
             return false;
@@ -68,70 +102,6 @@ export default defineConfig({
         }
       },
       transformMixedEsModules: true,
-    },
-  },
-  server: {
-    port: 4000,
-    proxy: {
-      "/api": {
-        target: "https://careapi.coronasafe.in",
-        changeOrigin: true,
-      },
-    },
-  },
-  esbuild: {
-    loader: "tsx",
-    include: [/src\/.*\.[tj]sx?$/],
-    exclude: [/src\/stories/],
-  },
-
-  define: {
-    // for unconventional usage of global by third party libraries
-    global: "window",
-  },
-  resolve: {
-    alias: [
-      {
-        // to revert the above workaround for jss-plugin used by material-ui
-        find: /^jss-plugin-(.*)$/,
-        replacement: "$1",
-        customResolver: (id) => {
-          if (id === "window") {
-            id = "global";
-          }
-          return resolve(
-            __dirname,
-            `./node_modules/jss-plugin-${id}/src/index.js`
-          );
-        },
-      },
-      {
-        // to revert the above wokraround for global-cache used by react-dates
-        find: /^(.*)-cache$/,
-        replacement: "$1",
-        customResolver: (id) => {
-          if (id === "window") {
-            id = "global";
-          }
-          return resolve(__dirname, `./node_modules/${id}-cache/index.js`);
-        },
-      },
-    ],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        // again thanks to thirdparty libraries for using jsx in js files
-        {
-          name: "load-js-files-as-jsx",
-          setup(build) {
-            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
-              loader: "jsx",
-              contents: await fs.readFile(args.path, "utf8"),
-            }));
-          },
-        },
-      ],
     },
   },
 });
